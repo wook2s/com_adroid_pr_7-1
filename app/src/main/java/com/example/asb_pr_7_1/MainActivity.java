782309f6cf2f12f9f7d949a5506453e0e388066d
jadeKim <jade0911k@gmail.com>
2022-04-26 20:59:32 +0900

    first commit

---
+++ app/src/main/java/com/example/asb_pr_7_1/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.asb_pr_7_1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout baseLayout;
+    Button btn1;
+    float ro = 0.0f;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("배경색 바꾸기");
+
+        baseLayout = (LinearLayout) findViewById(R.id.baseLayout);
+        btn1 = (Button) findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                baseLayout.setBackgroundColor(Color.WHITE);
+                ro = 0.0f;
+                btn1.setRotation(ro);
+                btn1.setScaleY(1);
+                btn1.setScaleX(1);
+
+            }
+        });
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu1, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemRed:
+                baseLayout.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.itemGreen:
+                baseLayout.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.itemBlue:
+                baseLayout.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.subRotate:
+                btn1.setRotation(ro+=45);
+                return true;
+            case R.id.subSize:
+                btn1.setScaleX(2);
+                btn1.setScaleY(2);
+                return true;
+        }
+
+        return false;
+    }
+}
